3ccff48fb4de9e047a2809a93e67f06456f38cd3
guivmc <guivmc@hotmail.com>
2019-07-31 09:01:51 -0300

    Create Singleton.java

---
+++ Singleton.java
@@ -0,0 +1,26 @@
+public class Singleton 
+{
+
+ //Create Instance
+	private static final Singleton instance = new Singleton();
+
+  //Constructor
+	private Singleton() {
+	}
+	
+	//Getter
+	public static Singleton getInstance() 
+	{
+		return instance;
+	}
+ }
+ 
+ public class Main
+ {
+
+	public static void main(String[] args) 
+  {
+      //Get Singleton
+      Singleton s = Singleton.getInstance(); 
+  }
+ }
